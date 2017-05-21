eb0ea1862f8d318a91fbdabf1f9542d32f6caf7e
gmoben <ben@warr.io>
2017-05-21 04:34:39 +0000

    Use string constants because I can't elisp + move org keybindings

--- .emacs.conf/config/settings/org.el
+++ .emacs.conf/config/settings/org.el
@@ -6,32 +6,6 @@
 (require 'org-clock)
 (require 'ox) ;; org-export http://orgmode.org/worg/exporters/ox-docstrings.html
 
-;; Key Bindings
-(global-set-key (kbd "C-c o a") 'org-agenda)
-(global-set-key (kbd "C-c o b") 'org-iswitchb) ;; TODO Find/write a helm plugin
-(global-set-key (kbd "C-c o c") 'org-capture)
-
-(define-key org-mode-map (kbd "C-c o h") 'org-insert-heading-after-current)
-(define-key org-mode-map (kbd "C-c o s") (lambda ()
-					   (interactive)
-					   (end-of-line)
-					   (org-insert-subheading nil))) ;; Insert after current line
-(define-key org-mode-map (kbd "C-c o t") 'org-insert-todo-heading)
-(define-key org-mode-map (kbd "C-c o p s") 'org-set-property)
-(define-key org-mode-map (kbd "C-c o p d") 'org-delete-property)
-
-;; Unmap add/remove files from org-agenda-files
-(define-key org-mode-map (kbd "C-c [") nil)
-(define-key org-mode-map (kbd "C-c ]") nil)
-
-;; Map RET to <f12> for iTerm
-(define-key org-mode-map (kbd "<C-^>") 'org-insert-heading-respect-content)
-(define-key org-mode-map (kbd "<f12>") 'org-return)
-(define-key org-mode-map (kbd "<M-f12>") 'org-meta-return)
-(define-key org-mode-map (kbd "<C-f12>") 'org-insert-heading-respect-content)
-(define-key org-mode-map (kbd "<C-S-f12>") 'org-insert-todo-heading-respect-content)
-(define-key org-mode-map (kbd "<M-S-f12>") 'org-insert-todo-heading)
-
 ;; Settings
 (org-clock-persistence-insinuate)
 (add-to-list 'auto-mode-alist '("\\.\\(org\\|org_archive\\|orgtmpl\\)$" . org-mode))
@@ -59,7 +33,7 @@
 
 ;; Add agenda files if directories exist
 (setq org-agenda-files (list))
-(dolist (path '(ben/org/ben ben/org/work) nil)
+(dolist (path '("~/org/ben" "~/org/work") nil)
   (if (or (file-exists-p path) (file-symlink-p path))
       (add-to-list 'org-agenda-files path)))
 
@@ -122,11 +96,11 @@
 ;; Conditionally set capture templates
 (setq org-capture-templates
       (let (tmpl (list))
-	(if (member ben/org/ben org-agenda-files)
+	(if (member "~/org/ben" org-agenda-files)
 	    (dolist (elem ben/org/capture/ben)
 	      (add-to-list 'tmpl elem 'append)))
 
-	(if (member ben/org/work org-agenda-files)
+	(if (member "~/org/work" org-agenda-files)
 	    (dolist (elem ben/org/capture/ben)
 	      (add-to-list 'tmpl elem 'append)))
 	tmpl))
@@ -142,6 +116,41 @@
   (not (member (nth 2 (org-heading-components)) org-done-keywords)))
 
 (setq org-refile-target-verify-function 'ben/verify-refile-target)
+
+;; Global Key Bindings
+(global-set-key (kbd "C-c o a") 'org-agenda)
+(global-set-key (kbd "C-c o b") 'org-iswitchb) ;; TODO Find/write a helm plugin
+(global-set-key (kbd "C-c o c") 'org-capture)
+
+(global-set-key (kbd "C-c o d") (lambda() (interactive) (find-file org-directory)))
+(global-set-key (kbd "C-c o q") (lambda() (interactive) (find-file ben/org/ben/quests)))
+(global-set-key (kbd "C-c o s") (lambda() (interactive) (find-file ben/org/ben/snippets)))
+(global-set-key (kbd "C-c o w q") (lambda() (interactive) (find-file ben/org/work/quests)))
+(global-set-key (kbd "C-c o w s") (lambda() (interactive) (find-file ben/org/work/snippets)))
+
+;; Org-mode bindings
+(define-key org-mode-map (kbd "C-c o h") 'org-insert-heading-after-current)
+(define-key org-mode-map (kbd "C-c o s") (lambda ()
+					   (interactive)
+					   (end-of-line)
+					   (org-insert-subheading nil))) ;; Insert after current line
+(define-key org-mode-map (kbd "C-c o t") 'org-insert-todo-heading)
+(define-key org-mode-map (kbd "C-c o p s") 'org-set-property)
+(define-key org-mode-map (kbd "C-c o p d") 'org-delete-property)
+
+;; Unmap add/remove files from org-agenda-files
+(define-key org-mode-map (kbd "C-c [") nil)
+(define-key org-mode-map (kbd "C-c ]") nil)
+
+;; Map RET to <f12> for iTerm
+(define-key org-mode-map (kbd "<C-return>") 'org-insert-heading-respect-content)
+(define-key org-mode-map (kbd "<C-^>") 'org-insert-heading-respect-content)
+(define-key org-mode-map (kbd "<f12>") 'org-return)
+(define-key org-mode-map (kbd "<M-f12>") 'org-meta-return)
+(define-key org-mode-map (kbd "<C-f12>") 'org-insert-heading-respect-content)
+(define-key org-mode-map (kbd "<C-S-f12>") 'org-insert-todo-heading-respect-content)
+(define-key org-mode-map (kbd "<M-S-f12>") 'org-insert-todo-heading)
+
 
 ;; (setq org-todo-state-tags-triggers
 ;;       '(("BLOCKED" ("BLOCKED" . t))
