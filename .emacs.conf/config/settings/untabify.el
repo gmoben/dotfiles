80f62f9cac931d35c5c296ab563a93813494678d
bwarren <bwarren@pindrop.com>
2018-08-13 14:52:46 -0400

    Untabify minor mode

---
+++ .emacs.conf/config/settings/untabify.el
@@ -0,0 +1,15 @@
+;; no tabs by default. modes that really need tabs should enable
+;; indent-tabs-mode explicitly. makefile-mode already does that, for
+;; example.
+(setq-default indent-tabs-mode nil)
+
+(defvar untabify-this-buffer)
+(defun untabify-all ()
+  "Untabify the current buffer, unless `untabify-this-buffer' is nil."
+  (and untabify-this-buffer (untabify (point-min) (point-max))))
+(define-minor-mode untabify-mode
+  "Untabify buffer on save." nil " untab" nil
+  (make-variable-buffer-local 'untabify-this-buffer)
+  (setq untabify-this-buffer (not (derived-mode-p 'makefile-mode)))
+  (add-hook 'before-save-hook #'untabify-all))
+(add-hook 'prog-mode-hook 'untabify-mode)
