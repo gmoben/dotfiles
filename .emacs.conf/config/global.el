dc868505d1f9b083666684bb56f7b1fbcbb62bb8
Benjamin Warren <ben@Benjamins-MacBook-Pro.local>
2019-08-10 23:47:24 -0700

    Emacs sort-words utility

--- .emacs.conf/config/global.el
+++ .emacs.conf/config/global.el
@@ -70,3 +70,15 @@ Position the cursor at it's beginning, according to the current mode."
        (if (file-exists-p filename)
            filename
          nil))
+
+;; https://www.emacswiki.org/emacs/SortWords
+(defun sort-words (reverse beg end)
+  "Sort words in region alphabetically, in REVERSE if negative.
+Prefixed with negative \\[universal-argument], sorts in reverse.
+
+The variable `sort-fold-case' determines whether alphabetic case
+affects the sort order.
+
+See `sort-regexp-fields'."
+  (interactive "*P\nr")
+  (sort-regexp-fields reverse "[\\w\\-_\\+]+" "\\&" beg end))
