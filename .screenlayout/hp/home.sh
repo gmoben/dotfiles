876838e3c5e766a9c71bd650485549c97fdc450b
bewarre <bewarre@amazon.com>
2020-03-19 22:07:42 -0700

    Theme updates

--- .screenlayout/hp/home.sh
+++ .screenlayout/hp/home.sh
@@ -1,64 +1,14 @@
 #!/usr/bin/env bash
 
-eDP="--mode 1920x1080 --pos 0x1088 --rotate normal"
-DP="--primary --mode 3840x1600 --pos 1920x0 --rate 30 --rotate normal"
+eDP="--mode 1920x1080 --pos 0x1080 --rotate normal"
+DP="--primary --mode 3840x1600 --rate 30 --pos 1920x0 --rotate normal"
 xrandr \
-    --output eDP-1 $eDP \
-    --output eDP-2 --off \
-    --output DP-1 $DP \
-    --output DP-2 --off \
+    --output eDP1 $eDP \
+    --output DP1 $DP \
+    --output HDMI1 --off \
+    --output HDMI2 --off \
     --output HDMI-1 --off \
-    --output HDMI-2 --off || \
-    xrandr \
-        --output eDP-1 $eDP \
-        --output eDP-2 --off \
-        --output DP-1 --off \
-        --output DP-2 $DP \
-        --output HDMI-1 --off \
-        --output HDMI-2 --off || \
-    xrandr \
-        --output eDP-1 $eDP \
-        --output eDP-2 --off \
-        --output DP-1 --off \
-        --output DP-2 --off \
-        --output HDMI-1 $DP \
-        --output HDMI-2 --off || \
-    xrandr \
-        --output eDP-1 $eDP \
-        --output eDP-2 --off \
-        --output DP-1 --off \
-        --output DP-2 --off \
-        --output HDMI-1 --off \
-        --output HDMI-2 $DP || \
-    xrandr \
-        --output eDP-1 --off \
-        --output eDP-2 $eDP \
-        --output DP-1 $DP \
-        --output DP-2 --off \
-        --output HDMI-1 --off \
-        --output HDMI-2 --off || \
-    xrandr \
-        --output eDP-1 --off \
-        --output eDP-2 $eDP \
-        --output DP-1 --off \
-        --output DP-2 $DP \
-        --output HDMI-1 --off \
-        --output HDMI-2 --off || \
-    xrandr \
-        --output eDP-1 --off \
-        --output eDP-2 $eDP \
-        --output DP-1 --off \
-        --output DP-2 --off \
-        --output HDMI-1 $DP \
-        --output HDMI-2 --off || \
-    xrandr \
-        --output eDP-1 --off \
-        --output eDP-2 $eDP \
-        --output DP-1 --off \
-        --output DP-2 --off \
-        --output HDMI-1 --off \
-        --output HDMI-2 $DP
-
+    --output HDMI-2 --off
 
 sleep 1
 
--- .screenlayout/hp/laptop.sh
+++ .screenlayout/hp/laptop.sh
@@ -3,15 +3,28 @@
 eDP="--primary --mode 1920x1080 --pos 0x1088 --rotate normal"
 DP="--mode 3840x1600 --pos 1920x0 --rate 30 --rotate normal"
 xrandr \
-    --output eDP-1 $eDP \
+    --output eDP1 $eDP \
+    --output eDP-1 --off \
     --output eDP-2 --off \
+    --output DP1 --off \
     --output DP-1 --off \
     --output DP-2 --off \
     --output HDMI-1 --off \
     --output HDMI-2 --off || \
     xrandr \
+        --output eDP1 --off \
+        --output eDP-1 $eDP \
+        --output eDP-2 --off \
+        --output DP1 --off \
+        --output DP-1 --off \
+        --output DP-2 --off \
+        --output HDMI-1 --off \
+        --output HDMI-2 --off || \
+    xrandr \
+        --output eDP1 --off \
         --output eDP-1 --off \
         --output eDP-2 $eDP \
+        --output DP1 --off \
         --output DP-1 --off \
         --output DP-2 --off \
         --output HDMI-1 --off \
