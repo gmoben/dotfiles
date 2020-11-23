3a86e88f3ec889e59d5900d372c9dbc2eadd28ea
bewarre <bewarre@amazon.com>
2020-11-23 01:11:32 -0800

    xrandr-apply now restarts i3 and actually resets nitrogen

--- .screenlayout/hp/docked.sh
+++ .screenlayout/hp/docked.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 declare -A params
-params[eDP-1]="--mode 1920x1080 --pos 3840x1376 --rotate normal"
-params[DP-2-1]="--primary --mode 3840x1600 --rate 30 --pos 0x0 --rotate normal"
-params[DP-2-2]=${params[DP-2-1]}
+params[eDP1]="--mode 1920x1080 --pos 3840x1376 --rotate normal"
+params[DP2-1]="--primary --mode 3840x1600 --rate 30 --pos 0x0 --rotate normal"
+params[DP2-2]=${params[DP2-1]}
 
 connected=$(xrandr -q | grep " connected " | awk '{print $1}')
 disconnected=$(xrandr -q | grep " disconnected " | awk '{print $1}')
