63b6a6ab3d75f2733b2b8695b5ee64d22da35e79
gmoben <ben@warr.io>
2025-11-17 15:05:44 -0800

    fix(nitrogen): send stderr to /dev/null when running wal-set

--- .config/wal/templates/set_nitrogen.sh
+++ .config/wal/templates/set_nitrogen.sh
@@ -1,4 +1,4 @@
 for i in $(seq 0 `xrandr --listactivemonitors | grep Monitors: | awk '{{print $2 - 1}}'`); do
     sleep 0.5
-    nitrogen --set-zoom-fill --head=$i "{wallpaper}"
+    nitrogen --set-zoom-fill --head=$i "{wallpaper}" 2>/dev/null
 done
