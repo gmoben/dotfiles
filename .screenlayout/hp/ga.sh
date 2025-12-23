6837d513bd2565b8bb72dccaafb740832961fb38
gmoben <ben@warr.io>
2025-12-22 18:30:17 -0800

    feat(i3): run dex on reload and fix config generation
    - Change dex from exec to exec_always so tray apps restart on i3 reload
    - Change wal-set to copy i3status-rust config instead of symlinking
      (avoids polluting dotfiles repo with absolute-path symlinks)
    - Add DP3-2 display aliases for HP ultrawide
    - Add nautilus to package list

--- .screenlayout/hp/ga.sh
+++ .screenlayout/hp/ga.sh
@@ -4,6 +4,8 @@ params[eDP1]="--mode 1920x1200 --pos 2219x1600 --rotate normal"
 params[eDP-1]=${params[eDP1]}
 params[DP1-2]="--primary --mode 3840x1600 --pos 0x0 --rotate normal"
 params[DP-1-2]=${params[DP1-2]}
+params[DP3-2]=${params[DP1-2]}
+params[DP-3-2]=${params[DP1-2]}
 
 connected=$(xrandr -q | grep " connected " | awk '{print $1}')
 disconnected=$(xrandr -q | grep " disconnected " | awk '{print $1}')
